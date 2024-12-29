6633cbef6b4aded0f62a00d76b717f73bdd12c6a
Blessing Ogunyinka <15864404+blessingogunyinka@users.noreply.github.com>
2024-12-29 01:05:02 -0500

    Update authRoutes.js no5
    lint corrections

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -13,9 +13,8 @@ router.put('/update', async (req, res) => {
   // Task 2: Validate the input using `validationResult` and return an appropriate message if you detect an error
   
   try {
-        
     const validationErrors = validationResult(req)
-
+    
     if (!validationErrors.isEmpty()) {
       logger.error('Update request has validation errors!', validationErrors.array())
       return res.status(400).json({ errors: validationErrors.array() })
@@ -26,15 +25,15 @@ router.put('/update', async (req, res) => {
     const email = req.headers.email 
 
     if (!email) {
-      logger.error("Email is not present in the request headers.")
-      return res.status(400).json({ error: "Email is not present in the request headers." })
+      logger.error('Email is not present in the request headers.')
+      return res.status(400).json({ error: 'Email is not present in the request headers.' })
     }
 
     // Task 4: Connect to MongoDB
 
     const db = await connectToDatabase()
     
-    const collection = db.collection("users")
+    const collection = db.collection('users')
 
     // Task 5: Find the user credentials in database
 
@@ -53,16 +52,16 @@ router.put('/update', async (req, res) => {
     }
 
     if (req.body.password) {
-      const salt = await bcryptjs.genSalt(10) ; 
+      const salt = await bcryptjs.genSalt(10)
       const hash = await bcryptjs.hash(req.body.password, salt)
-      existingUser.password = hash ; 
+      existingUser.password = hash
     }
 
     const updatedUser = await collection.findOneAndUpdate(
       { email },
       { $set: existingUser },
       { returnDocument: "after" }
-    ) ; 
+    )
 
     // Task 7: Create JWT authentication with `user._id` as a payload using the secret key from the .env file
 
@@ -90,15 +89,15 @@ router.post('/register', async (req, res) => {
 
     // Task 2: Access MongoDB `users` collection
 
-    const collection = db.collection("users")
+    const collection = db.collection('users')
 
     // Task 3: Check if user credentials already exists in the database and throw an error if they do
 
     const email = await collection.findOne({ email: req.body.email })
 
     if (email) {
-      logger.error("User already exists with this email")
-      return res.status(400).json({ error: "User already exists with this email" })
+      logger.error('User already exists with this email')
+      return res.status(400).json({ error: 'User already exists with this email' })
     }
 
     // Task 4: Create a hash to encrypt the password so that it is not readable in the database
@@ -127,15 +126,15 @@ router.post('/register', async (req, res) => {
 
     // Task 7: Log the successful registration using the logger
 
-    logger.info("User successfully registered!")
+    logger.info('User successfully registered!')
 
     // Task 8: Return the user email and the token as a JSON
 
     res.status(200).json({ authtoken: authtoken, email: req.body.email })
 
   } catch (e) {
-    console.log("Error: ", e)
-    return res.status(500).send("Internal server error")
+    console.log('Error: ', e)
+    return res.status(500).send('Internal server error')
   }
 
 }) ;
@@ -150,7 +149,7 @@ router.post('/login', async (req, res) => {
 
     // Task 2: Access MongoDB `users` collection
 
-    const collection = db.collection("users")
+    const collection = db.collection('users')
 
     // Task 3: Check for user credentials in database
 
@@ -163,8 +162,8 @@ router.post('/login', async (req, res) => {
       const passwordMatch = await bcryptjs.compare(req.body.password, userLoggingIn.password)
 
       if (!passwordMatch) {
-          logger.error("The passwords don't match!")
-          return res.status(404).json({ error: "Incorrect password" })              
+          logger.error('The passwords don't match!')
+          return res.status(404).json({ error: 'Incorrect password' })              
       }
 
       // Task 5: Fetch user details from a database
@@ -184,12 +183,12 @@ router.post('/login', async (req, res) => {
 
       // Task 7: Send appropriate message if the user is not found
     } else {
-      logger.error("User was not found")
-      return res.status(404).json({ error: "User was not found" })
+      logger.error('User was not found')
+      return res.status(404).json({ error: 'User was not found' })
     }
 
   } catch (e) {
-    return res.status(500).send("Internal server error")
+    return res.status(500).send('Internal server error')
   }
 }) ; 
 
