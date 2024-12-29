a4851dd48c8e8f062dd2a2a2b82789f01525150b
Blessing Ogunyinka <15864404+blessingogunyinka@users.noreply.github.com>
2024-12-28 23:45:45 -0500

    Update app.js
    moe lint corrections

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -4,14 +4,12 @@ const express = require('express')
 const cors = require('cors')
 const pinoLogger = require('./logger')
 
-
 const connectToDatabase = require('./models/db')
-const { loadData } = require("./util/import-mongo/index");
+// const { loadData } = require('./util/import-mongo/index');
 
-
-const app = express();
-app.use("*",cors());
-const port = 3060;
+const app = express()
+app.use('*', cors())
+const port = 3060
 
 // Connect to MongoDB; we just do this one time
 connectToDatabase().then(() => {
